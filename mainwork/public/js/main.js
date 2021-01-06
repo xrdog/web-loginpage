e18fbeacb70f575d52782766fac2bdf3a8bfcbea
罗龙光 <luolongguang@bytedance.com>
2021-01-06 17:11:38 +0800

    基本功能实现

--- mainwork/public/js/main.js
+++ mainwork/public/js/main.js
@@ -1,3 +1,4 @@
+let loginSuccessUrl='http://127.0.0.1:8081/loginsuccess'
 function login() {
     let username=document.getElementById("loginUsername").value;
     let userpasswd=document.getElementById("loginUserpasswd").value;
@@ -7,8 +8,56 @@ function login() {
         //alert(xhr.status);
         if(xhr.readyState==4){
             if(xhr.status==200||xhr.status==304){
-                var data = xhr.responseText;
+                let data = xhr.responseText;
+
+                (function(win,doc) {
+                    var alert = function(text, time, top) {
+                        text = text || "???？",time = time || 3000,top = top || "15%";//增加默认值，增强健壮性
+                        var body=doc.getElementsByTagName("body")[0];//优化dom
+                        //实现alert
+                        var div = doc.createElement("div");
+                        div.style.backgroundColor = " #22b9ff";
+                        div.style.color = " #fff";
+                        div.style.position = " fixed";
+                        div.style.zIndex = 9999999;
+                        div.style.height = " 60px";
+                        div.style.top = top;
+                        div.style.left = "83%";
+                        div.style.lineHeight = " 60px";
+                        div.style.borderRadius = " 4px";
+                        div.style.fontSize = " 20px";
+                        div.style.textAlign = "center";
+                        div.style.padding = "0 10px";
+                        div.className = "animated  bounceInDown";
+                        div.id = "alert";
+                        div.innerHTML = text;
+                        body.appendChild(div);
+                        var selfObj = doc.getElementById("alert");
+                        //动态调整位置
+                        var alertWidth = win.getComputedStyle(selfObj, null).width;
+                        div.style.marginLeft = -parseInt(alertWidth) / 2 + "px";
+                        setTimeout(function() {
+                            body.removeChild(div);
+                            if (data==="登录成功")
+                            {
+                                console.log('登录成功');
+                                window.location.href=loginSuccessUrl;
+                            }
+                        }, time);
+                    }
+                    win.alert=alert;//导出
+                })(window,document);
+                alert(data,1500);
+            
+                /* alert(data);
+                window.location.href=loginSuccessUrl; */
             }
+            else {
+                alert("网络通讯错误-not 200|304");
+            }
+        }
+        else {
+            //alert("网络通讯错误-not ready");
         }
     }
 
@@ -17,3 +66,69 @@ function login() {
     console.log(sendStr);
     xhr.send();
 }
+
+function register() {
+    let username=document.getElementById("loginUsername").value;
+    let userpasswd=document.getElementById("loginUserpasswd").value;
+    let xhr=new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+        //alert(xhr.status);
+        if(xhr.readyState==4){
+            if(xhr.status==200||xhr.status==304){
+                let data = xhr.responseText;
+
+                (function(win,doc) {
+                    var alert = function(text, time, top) {
+                        text = text || "???？",time = time || 3000,top = top || "15%";//增加默认值，增强健壮性
+                        var body=doc.getElementsByTagName("body")[0];//优化dom
+                        //实现alert
+                        var div = doc.createElement("div");
+                        div.style.backgroundColor = " #22b9ff";
+                        div.style.color = " #fff";
+                        div.style.position = " fixed";
+                        div.style.zIndex = 9999999;
+                        div.style.height = " 60px";
+                        div.style.top = top;
+                        div.style.left = "83%";
+                        div.style.lineHeight = " 60px";
+                        div.style.borderRadius = " 4px";
+                        div.style.fontSize = " 20px";
+                        div.style.textAlign = "center";
+                        div.style.padding = "0 10px";
+                        div.className = "animated  bounceInDown";
+                        div.id = "alert";
+                        div.innerHTML = text;
+                        body.appendChild(div);
+                        var selfObj = doc.getElementById("alert");
+                        //动态调整位置
+                        var alertWidth = win.getComputedStyle(selfObj, null).width;
+                        div.style.marginLeft = -parseInt(alertWidth) / 2 + "px";
+                        setTimeout(function() {
+                            body.removeChild(div);
+                            if (data==="passwdcorrect")
+                                window.location.href=loginSuccessUrl;
+                        }, time);
+                    }
+                    win.alert=alert;//导出
+                })(window,document);
+                alert(data,3000);
+            
+                /* alert(data);
+                window.location.href=loginSuccessUrl; */
+            }
+            else {
+                alert("网络通讯错误-not 200|304");
+            }
+        }
+        else {
+            //alert("网络通讯错误-not ready");
+        }
+    }
+
+    let sendStr='register?username='+username+'&'+'userpasswd='+userpasswd;
+    xhr.open("GET",sendStr,true);
+    console.log(sendStr);
+    xhr.send();
+}
+
--- mainwork/service.js
+++ mainwork/service.js
@@ -4,18 +4,19 @@ let app = express();
 let bodyParser = require('body-parser');
 
 var mysql      = require('mysql');
+const { query } = require('express');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '123456',
-  database : 'mysql'
+  database : 'xrdoglogin'
 });
 
 connection.connect();
- 
+
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+  console.log('The solution is: ', results[0]);
 });
 
 // 创建 application/x-www-form-urlencoded 编码解析
@@ -27,14 +28,83 @@ app.get('/index.html', function (req, res) {
   console.log('dirname',__dirname);
    res.sendFile( __dirname + "/" + "index.html" );
 })
-  
+
+app.get('/loginsuccess', function (req, res) {
+  console.log('dirname',__dirname);
+   res.sendFile( __dirname + "/" + "loginsuccess.html" );
+})
+
 app.get('/login', function (req, res) {
  
     // 输出 JSON 格式
-    console.log(req.query);
-    res.end();
+    let querysql='select passwd from userinfo where username="'+req.query.username+'"';
+    console.log(querysql);
+    connection.query(querysql,function(err, result) {
+      if(err){
+        console.log('[SELECT ERROR] - ',err.message);
+      }
+      else{
+            if (result.length) {
+              console.log('--------------------------SELECT----------------------------');
+              sqlpasswd=result[0].passwd;
+              console.log(req.query);
+              console.log(sqlpasswd);//拿到指定用户名的密码
+              console.log(req.query.userpasswd);//待比较的密码
+              console.log('------------------------------------------------------------\n\n');
+              if (sqlpasswd == req.query.userpasswd) {
+                res.end("登录成功");
+              } //密码正确
+              else {
+                res.end("密码错误")
+              } 
+            }
+            else {
+              //用户不存在
+              res.end("用户不存在");
+            }
+      }
+    });
+    
  })
 
+ app.get('/register', function (req, res) {
+ 
+  // 输出 JSON 格式
+  let querysql='select passwd from userinfo where username="'+req.query.username+'"';
+  console.log(querysql);
+  connection.query(querysql,function(err, result) {
+    if(err){
+      console.log('[SELECT ERROR] - ',err.message);
+    }
+    else{
+          if (result.length) {
+            console.log('--------------------------SELECT----------------------------');
+            sqlpasswd=result[0].passwd;
+            console.log(req.query);
+            console.log(sqlpasswd);//拿到指定用户名的密码
+            console.log(req.query.userpasswd);//待比较的密码
+            console.log('------------------------------------------------------------\n\n');
+            res.end("用户已存在，暂不支持重复注册与修改");
+          }
+          else {
+            //用户不存在
+            let insertsql=`INSERT INTO userinfo (username,passwd) values ("${req.query.username}","${req.query.userpasswd}")`;
+            console.log('insertsql:',insertsql);
+            connection.query(insertsql,function(err,data){
+              if(err){
+                  console.log('插入数据失败')
+                  res.end("注册失败");
+              } else {
+                  console.log('插入数据成功');
+                  res.end("注册成功");
+              }
+            }
+          );
+        }
+    }
+  });
+  
+})
 var server = app.listen(8081, function () {
   console.log("应用实例，访问地址为 http://127.0.0.1:8081/index");
 })
